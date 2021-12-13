72fa7281535d73cfe3dad3ac77ec4d7e7be0d6f9
ujanmdr <umdr98@gmail.com>
2021-12-13 20:37:48 +0545

    adding props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,40 @@ import './App.css';
 import ExpenseItems from '../src/component/ExpenseItems';
 
 function App() {
+  const expenses = [
+    {
+      id:'1',
+      title:'ToiletPaper',
+      amount:120,
+      date: new Date(2021,12,13),
+    },
+    {
+      id:'2',
+      title:'Eraser',
+      amount:10,
+      date: new Date(2021,12,13),
+    },
+    {
+      id:'3',
+      title:'Paper',
+      amount:50,
+      date: new Date(2021,12,13),
+    },
+    {
+      id:'4',
+      title:'Pen',
+      amount:100,
+      date: new Date(2021,12,13),
+    },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItems></ExpenseItems> 
-  
+      <ExpenseItems title ={expenses[0].title} amount ={expenses[0].amount} date ={expenses[0].date} ></ExpenseItems> 
+      <ExpenseItems title ={expenses[1].title} amount ={expenses[1].amount} date ={expenses[1].date} ></ExpenseItems> 
+      <ExpenseItems title ={expenses[2].title} amount ={expenses[2].amount} date ={expenses[2].date} ></ExpenseItems> 
+      <ExpenseItems title ={expenses[3].title} amount ={expenses[3].amount} date ={expenses[3].date} ></ExpenseItems> 
+
     </div>
   );
 }
--- src/component/ExpenseItems.js
+++ src/component/ExpenseItems.js
@@ -2,17 +2,14 @@ import React from "react";
 import ".//css/ExpenseItems.css"
 
 
-function ExpenseItems(){
-    const expenseDate = new Date (2021, 12, 13);
-    const expenseTitle = ("Car Insurance");
-    const expensePrice = 12000
-
+function ExpenseItems(props){
+    
     return(
     <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">Rs{expensePrice}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">Rs{props.amount}</div>
         </div>
     </div>
     );
