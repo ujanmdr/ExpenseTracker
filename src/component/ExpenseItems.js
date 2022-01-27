13876422d66f25aa8d1c266c214ae51181f1cc39
ujanmdr <umdr98@gmail.com>
2022-01-27 12:36:45 +0545

    Added js Card wraping

--- src/component/ExpenseItems.js
+++ src/component/ExpenseItems.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ".//css/ExpenseItems.css"
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card"
 
 
 function ExpenseItems(props){
     
     return(
-    <div className="expense-item">
+    <Card className="expense-item">
         <ExpenseDate date = {props.date} />
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">Rs {props.amount}</div>
         </div>
-    </div>
+    </Card>
     );
 }
 
